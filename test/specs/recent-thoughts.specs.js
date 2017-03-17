2c92355735421b6248a7fe4871f1688dcfe8261c
Ryan Cronin <Ryan@Ryans-MacBook-Pro.local>
2017-03-17 08:49:55 -0400

    5 tests complete

---
+++ test/specs/recent-thoughts.specs.js
@@ -0,0 +1,55 @@
+(function() {
+  'use strict';
+  let expect = chai.expect;
+  describe('Main module', function(){
+
+    describe('showRecent function', function(){
+
+      beforeEach(function(){
+        let main = document.createElement('main');
+        main.classList.add('recent');
+        document.querySelector('body').appendChild(main);
+      });
+
+      afterEach(function(){
+        let main = document.querySelector('main');
+        main.parentNode.removeChild(main);
+      });
+
+      it('The argument isnt an array', function(){
+        let result = window.thoughter.showRecent('hello');
+        expect(result).to.be.undefined;
+      });
+
+      it('should be a function', function(){
+        expect(window.thoughter.showRecent).to.be.a.function;
+      });
+
+      it('should run the function', function(){
+        window.thoughter.showRecent([
+          {content:'This isnt easy', createTime: '09:00', id: 'Ryan'}
+        ]);
+        let articles = document.querySelectorAll('main article');
+        expect(articles.length).to.equal(1);
+      });
+
+      it('should handle an empty array', function(){
+        let result = window.thoughter.showRecent([]);
+        expect(window.thoughter.showRecent.length).to.equal(0);
+      });
+      
+      it('should handle an array of objects that do not meet the criteria', function(){
+        window.thoughter.showRecent([{content: 'ryan', number: 10 }]);
+        let articles = document.querySelectorAll('main article');
+        expect(articles.length).to.equal(0);
+      });
+
+      it('should add element for one piece of data', function(){
+        window.thoughter.showRecent([{content: 'Ryan'}]);
+        let articles = document.querySelectorAll('main article');
+        expect(articles.length).to.equal(0);
+      });
+
+    });
+  });
+}());
